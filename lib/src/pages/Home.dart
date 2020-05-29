a4350400c04a229521e8c679527be7679f73f1d3
jdcr1425 <juandavidcr7@gmail.com>
2020-05-28 23:59:46 -0500

    fixing some design bugs

--- lib/src/pages/Home.dart
+++ lib/src/pages/Home.dart
@@ -73,6 +73,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text('Olores'),
         centerTitle: true,
--- lib/src/pages/Personal_information_page.dart
+++ lib/src/pages/Personal_information_page.dart
@@ -10,7 +10,7 @@ class PersonalInformation extends StatefulWidget {
 }
 
 class _PersonalInformationState extends State<PersonalInformation> {
-  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _genderValue = 'Masculino';
   String _name = "";
@@ -28,164 +28,163 @@ class _PersonalInformationState extends State<PersonalInformation> {
     prefs.setString('user', person);
   }
 
+  Widget _buildName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Nombre'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'El nombre es requerido';
+        }
+      },
+      onSaved: (String value) {
+        _name = value;
+      },
+    );
+  }
+
+  Widget _buildAge() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Edad'),
+      keyboardType: TextInputType.number,
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'La edad es requerida';
+        } else {
+          final number = num.tryParse(value);
+          if (number == null) {
+            return '"$value" no es un numero válido';
+          }
+        }
+      },
+      onSaved: (value) {
+        _age = int.parse(value);
+      },
+    );
+  }
+
+  Widget _buildGender() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "Sexo",
+          style: TextStyle(fontSize: 16.0, color: Colors.grey[600]),
+        ),
+        DropdownButton<String>(
+            value: _genderValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Color.fromRGBO(19, 48, 119, 1.0)),
+            underline: Container(
+              height: 2,
+              color: Color.fromRGBO(19, 48, 119, 1.0),
+            ),
+            onChanged: (String valure) {
+              setState(() {
+                _genderValue = valure;
+              });
+            },
+            items: <String>['Masculino', 'Femenino']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList())
+      ],
+    );
+  }
+
+  Widget _buildProfession() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'profesión'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'La profesión es requerida';
+        }
+      },
+      onSaved: (String value) {
+        _profession = value;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: Align(
-            child: Material(
-          color: Colors.white,
-          elevation: 14.0,
-          borderRadius: BorderRadius.circular(24.0),
-          shadowColor: Color(0x802196f3),
-          child: Container(
-            width: 350.0,
-            height: 420.0,
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 20,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Color(0xfff0f0f0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, blurRadius: 10.0, offset: Offset(5, 5))
+            ]),
+        child: Form(
+            key: _formKey,
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Center(
-                            child: Text("Regístrate",
-                                style: TextStyle(
-                                    color: Colors.grey[800],
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 29))),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
-                          decoration:
-                              InputDecoration(hintText: 'Digite su nombre'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Digite su nombre';
-                            } else {
-                              this._name = value;
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        TextFormField(
-                          decoration:
-                              InputDecoration(hintText: 'Digite su edad'),
-                          keyboardType: TextInputType.number,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Digite su edad';
-                            }
-                            final n = num.tryParse(value);
-                            if (n == null) {
-                              return '"$value" no es un numero válido';
-                            } else {
-                              this._age = n;
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "Sexo",
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                            SizedBox(
-                              width: 10.0,
-                            ),
-                            DropdownButton(
-                                value: _genderValue,
-                                icon: Icon(
-                                  Icons.arrow_downward,
-                                  color: Colors.orange,
-                                ),
-                                iconSize: 24,
-                                elevation: 20,
-                                onChanged: (String newValue) {
-                                  setState(() {
-                                    _genderValue = newValue;
-                                  });
-                                },
-                                items: <String>[
-                                  'Masculino',
-                                  'Femenino'
-                                ].map<DropdownMenuItem<String>>((String value) {
-                                  return DropdownMenuItem<String>(
-                                    value: value,
-                                    child: Text(value),
-                                  );
-                                }).toList())
-                          ],
-                        ),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        TextFormField(
-                          decoration:
-                              InputDecoration(hintText: 'Digite su profesión'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Digite su profesión';
-                            } else {
-                              this._profession = value;
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(
-                          height: 30.0,
-                        ),
-                        Center(
-                            child: Container(
-                          width: 110.0,
-                          height: 50.0,
-                          child: FlatButton(
-                            color: Colors.orange,
-                            onPressed: () {
-                              if (_formKey.currentState.validate()) {
+                Text("Datos personales",
+                    style: TextStyle(
+                        color: Colors.grey[800],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22)),
+                _buildName(),
+                _buildAge(),
+                _buildGender(),
+                _buildProfession(),
+                SizedBox(
+                  height: 10,
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    if (!_formKey.currentState.validate()) {
+                      return;
+                    }
+                    _formKey.currentState.save();
 
-                                var person = {
-                                  "Name": this._name,
-                                  "Age": this._age,
-                                  "Gender": this._genderValue,
-                                  "Profession": this._profession
-                                };
+                    var person = {
+                      "Name": this._name,
+                      "Age": this._age,
+                      "Gender": this._genderValue,
+                      "Profession": this._profession
+                    };
 
-                                String user = jsonEncode(person);
-                                _saveUser(user);
+                    String user = jsonEncode(person);
+                    _saveUser(user);
 
-                                Scaffold.of(context).showSnackBar(SnackBar(
-                                    content: Text('Registro exitoso')));
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Guardado exitoso')));
 
-                                Navigator.pushNamed(context, '/');
-                              }
-                            },
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  Icon(Icons.save),
-                                  Text("Guardar")
-                                ]),
-                          ),
-                        ))
-                      ],
-                    ),
+                    Navigator.pushReplacementNamed(context, '/');
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.save,
+                        color: Color.fromRGBO(19, 48, 119, 1.0),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Guardar")
+                    ],
                   ),
                 )
               ],
-            ),
-          ),
-        )),
+            )),
       ),
     );
   }
