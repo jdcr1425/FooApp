3b10574b0f4344a7bcb7328fd799db4a6935dfa9
jdcr1425 <juandavidcr7@gmail.com>
2020-06-02 03:18:38 -0500

    adding google sheet integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:olores/src/app.dart';
  
-void main() => runApp(DevicePreview(builder: (context) => MyApp() ) );
+//void main() => runApp(DevicePreview(builder: (context) => MyApp() ) );
 
-//void main() => runApp( MyApp()) ;
+void main() => runApp( MyApp()) ;
  
--- lib/src/provider/olores_provider.dart
+++ lib/src/provider/olores_provider.dart
@@ -5,10 +5,21 @@ import 'package:olores/src/models/Olor_model.dart';
 
 class OloresProvider {
   final String _url = 'https://olores-4ad6a.firebaseio.com';
+  final String URL =
+      "https://script.google.com/macros/s/AKfycbxAhc0J3V7DgiDmOIkWYHpWeQKI1DxDfAFpmV8YMnK31FhFpRw/exec";
+
+  String _toParams(OlorModelo olor) {
+    return "?olor=${olor.nombreOlor[0]}&lugar=${olor.lugar}&intesidad=${olor.intensidad}&nivel_distincion=${olor.nivelDistincion}&sentimiento=${olor.sentimiento}&nivel_afectacion=${olor.nivelAfectacion}&olor_resulta=${olor.nivelSatisfaccion}&emocion=${olor.nivelEmocion[0]}&reaccion=${olor.reaccion[0]}&fecha=${olor.fecha}&temperatura=${olor.temperatura}&coordenadas=${olor.coordenadas[0]} , ${olor.coordenadas[1]}&nombre=${olor.personalInformation[0]}&edad=${olor.personalInformation[1]}&profesion=${olor.personalInformation[3]}&sexo=${olor.personalInformation[2]}";
+  }
 
   Future<bool> crearRegistroOlor(OlorModelo olor) async {
-    final url = '$_url/olores.json';
-    final resp = await http.post(url, body: olorModeloToJson(olor));
+    //final url = '$_url/olores.json';
+    final url = URL + _toParams(olor);
+
+    print(url);
+    //final resp = await http.post(url, body: olorModeloToJson(olor));
+
+    final resp = await http.get(url);
 
     final decodedData = json.decode(resp.body);
 
